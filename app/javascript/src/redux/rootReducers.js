3f8c7c69d44d019261d13cc924dde971b1fc561a
BHObserver <BHObserver0@gmail.com>
2024-02-12 00:29:41 +0600

    delete javascript folder

--- app/javascript/src/redux/rootReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import randomMessage from './welcome/greetingsSlice';
-
-const rootReducer = combineReducers({
-  Random: randomMessage,
-});
-
-export default rootReducer;
