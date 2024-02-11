a1b920109c97acbe6d64c170d9bb1f293539b0f2
BHObserver <BHObserver0@gmail.com>
2024-02-12 00:30:21 +0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'greetings/index'
+    end
+  end
+  root 'root#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  get 'random_greeting', to: 'messages#random'
-
-  # Route the root path to a controller action that will render your React application
-  root 'pages#index'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
 end
